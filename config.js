2b2d36f052046b7f42dfb9938e8b9ec8364089c2
takeshimonoseki <takeshimonoseki@gmail.com>
2026-02-12 21:10:01 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// drivers-site 共通設定
+// GitHub Pages ルート: https://takeshimonoseki.github.io/
+// このディレクトリ（drivers/）配下に index.html / register.html / vehicle.html などを配置
+window.CONFIG = {
+  REGION_LABEL: "山口県",
+  REGIONS: ["山口県", "福岡県"],
+  COMPANY_NAME: "軽貨物TAKE",
+  TEL: "09078834125",
+  LINE_URL: "https://line.me/ti/p/CJSHrsA4cb",
+  EMAIL_TO: "takeshimonoseki@gmail.com",
+  // GitHub Pages から GAS へのエンドポイント
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbwzSFlI7k_eUkHzCyMY8isEyhOCSMrdAovFM2g7XGFZAfA9_3LS2J7F-HNIb5RtCkrAqg/exec",
+  // 一括で差し替えたいとき用にコメントとして SITE_BASE_URL を残しておく
+  // SITE_BASE_URL: "https://takeshimonoseki.github.io/",
+  MAX_FILE_MB: 5
+};
